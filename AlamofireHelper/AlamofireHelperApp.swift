f0465859080c746b4a1792ab83c737be1eed0e12
Tushar <tusharj3255@gmail.com>
2026-01-21 09:52:19 +0530

    Initial Commit

---
+++ AlamofireHelper/AlamofireHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireHelperApp.swift
+//  AlamofireHelper
+//
+//  Created by Prakhar Jaiswal on 21/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
